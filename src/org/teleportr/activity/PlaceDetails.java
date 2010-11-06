6e5c3549017d46328a6d06e813a996de0d8c0779
orangeman <flo@andlabs.de>
2010-11-06 01:04:12 +0100

    finish on inputmethod done

--- src/org/teleportr/activity/PlaceDetails.java
+++ src/org/teleportr/activity/PlaceDetails.java
@@ -9,13 +9,17 @@ import org.teleportr.model.Place;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.KeyEvent;
 import android.view.View;
 import android.view.View.OnClickListener;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.SlidingDrawer;
 import android.widget.TextView;
+import android.widget.TextView.OnEditorActionListener;
 
-public class PlaceDetails extends Activity {
+public class PlaceDetails extends Activity implements OnEditorActionListener {
 
 
 	private Place place;
@@ -35,8 +39,10 @@ public class PlaceDetails extends Activity {
 
 //	    if (place.name != null)
 	    	((TextView)findViewById(R.id.name)).setText(place.name);
+	    	((TextView)findViewById(R.id.name)).setOnEditorActionListener(this);
 //	    if (place.city != null)
 	    	((TextView)findViewById(R.id.city)).setText(place.city);
+	    	((TextView)findViewById(R.id.city)).setOnEditorActionListener(this);
 	    if (place.icon != 0)
 	    	((ImageView)findViewById(R.id.icon)).setImageResource(place.icon);
 	    else
@@ -48,37 +54,34 @@ public class PlaceDetails extends Activity {
 	    		((TextView)findViewById(R.id.nmb)).setText(address.group(2));
 	    	} else {
 	    		((TextView)findViewById(R.id.street)).setText(place.address);
-	    		((TextView)findViewById(R.id.nmb)).setText("??");
 	    	}
 	    }
-	    ((TextView)findViewById(R.id.latlon)).setText(place.lat+"\n"+place.lon);
-
-	    findViewById(R.id.ok).setOnClickListener(new OnClickListener() {
-
-	    	@Override
-	    	public void onClick(View v) {
-	    		place.address = ((EditText)findViewById(R.id.street)).getText().toString()+
-	    		" "+((EditText)findViewById(R.id.nmb)).getText().toString();
-	    		place.name = ((EditText)findViewById(R.id.name)).getText().toString();
-	    		place.city = ((EditText)findViewById(R.id.city)).getText().toString();
-	    		if (place.name.equals(""))
-	    			place.name = place.address;
-	    		if (place.name.equals(""))
-	    			place.name = "Start";
-	    		setResult(RESULT_OK, getIntent().putExtra("place", place));
-	    		finish();
-	    	}
-	    });
+	    ((TextView)findViewById(R.id.street)).setOnEditorActionListener(this);
+	    
+//	    ((TextView)findViewById(R.id.latlon)).setText(place.lat+"\n"+place.lon);
 
 	}
-
-	
 
 	@Override
-	protected void onPause() {
-		super.onPause();
+	public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
+		finish();
+		return true;
 	}
 
+	@Override
+	public void finish() {
+		// TODO Auto-generated method stub
+		place.address = ((EditText)findViewById(R.id.street)).getText().toString()+
+		" "+((EditText)findViewById(R.id.nmb)).getText().toString();
+		place.name = ((EditText)findViewById(R.id.name)).getText().toString();
+		place.city = ((EditText)findViewById(R.id.city)).getText().toString();
+		if (place.name.equals(""))
+			place.name = place.address;
+		if (place.name.equals(""))
+			place.name = "Start";
+		setResult(RESULT_OK, getIntent().putExtra("place", place));
+		super.finish();
+	}
 	
 	
 }
